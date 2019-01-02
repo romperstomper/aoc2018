2bd59e23b18608495aa435bbc01cb069f166da04
gary boland <garyboland@gmail.com>
2019-01-02 22:58:12 +0000

    working DFS solution

--- aoc25/aoc25.py
+++ aoc25/aoc25.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import itertools
 import pdb
+from collections import deque
 DATA0 = [
 [0,0,0,0],
 [3,0,0,0],
@@ -14,39 +15,47 @@ DATA0 = [
 DATA=[
 [0,0,0,6],
 [9,0,0,0],
+[12,0,0,0]
 ]
 def readfileinput():
-    cords = namedtuple("Cords", ['w', 'x', 'y', 'z'])
-
     result = list()
     with open('input') as fd:
         data = fd.read().splitlines()
     for line in data:
-        result.append(list(map(int, cords(*line.split(',')))))
+        result.append(list(map(int, line.split(','))))
     return formatter(result)
 
-def manhattan(a, b):
-    w,x,y,z = a
-    w2,x2,y2,z2 = b
-    if abs(w - w2) <=3 or abs(x - x2) <=3 or abs(y - y2) <=3 or abs(z - z2) <=3:
-        print(abs(w - w2) <=3, abs(x - x2) <=3, abs(y - y2) <=3, abs(z - z2)<=3)
-        print(abs(w - w2), abs(x - x2) <=3, abs(y - y2) <=3, abs(z - z2)<=3)
-        return True
-    return False
-
-def countconstellations(cords):
-    cords = formatter(cords)
+def creategraph(cords):
     result = []
-    for cord in cords:
-        tmplist = []
-        result.append(tmplist)
-        for i in cords:
-            #pdb.set_trace()
-            if manhattan(cord, i):
-                raise
-                tmplist.append(i)
-    
-    return result
+    edges = [set() for _ in range(len(cords))]
+    for i,(w,x,y,z) in enumerate(cords):
+        for j,(w2,x2,y2,z2) in enumerate(cords):
+            if (abs(w - w2)+abs(x - x2)+abs(y - y2)+abs(z - z2) <=3):
+                edges[i].add(j)
+                #print(cords[i],cords[j],abs(w - w2)+abs(x - x2)+abs(y - y2)+abs(z - z2))
+            #else:
+                #print(cords[i],cords[j],abs(w - w2)+abs(x - x2)+abs(y - y2)+abs(z - z2))
+    return edges
+
+def counter(cords):
+    graph = creategraph(cords)
+    Set = set()
+    ans = 0
+    l = len(cords)
+    for i in range(l):
+        if i in Set:
+            continue
+        ans +=1
+        Q = deque()
+        Q.append(i)
+        while Q:
+            x = Q.popleft()
+            if x in Set:
+                continue
+            Set.add(x)
+            for y in graph[x]:
+                Q.append(y)
+    return ans
 
 def formatter(data):
     result = list()
@@ -64,13 +73,13 @@ DATA2 = [
 [-1,3,2,2],
 [-1,0,-1,0],
 [0,2,1,-2],
-[3,0,0,0 ],
+[3,0,0,0],
 ]          
 DATA3=[
 [1,-1,0,1],
 [2,0,-1,0],
 [3,2,-1,0],
-[0,0,3,1 ],
+[0,0,3,1],
 [0,0,-1,-1],
 [2,3,-2,0],
 [-2,2,0,0],
--- aoc25/test_aoc25.py
+++ aoc25/test_aoc25.py
@@ -2,14 +2,12 @@ import unittest
 import aoc25
 
 class Test25(unittest.TestCase):
-    def test_checkrange(self):
-        data = aoc25.DATA
-        self.assertEqual(aoc25.manhattan(data[0], data[1]), False)
-        self.assertTrue(aoc25.manhattan(data[1], data[2]), True)
-
-    def est_countconstellations(self):
-        pass
-        #res = aoc25.countconstellations(aoc25.DATA)
+    def test_countconstellations(self):
+        data = aoc25.readfileinput()
+        #self.assertEqual(aoc25.counter(data), 359)
+        self.assertEqual(aoc25.counter(aoc25.DATA0), 2)
+        #self.assertEqual(aoc25.creategraph(aoc25.DATA0), 2)
+        #self.assertEqual(aoc25.creategraph(aoc25.DATA0), 2)
 
 if __name__ == '__main__':
     unittest.main()
