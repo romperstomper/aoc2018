de8888c5d4e8f5e4029d6e0d75077a8c871866ac
Gary Boland <gary.boland@ALLFOURTHASSIGN.workdayinternal.com>
2019-10-30 11:29:56 +0000

    update

--- aoc6/aoc6.py
+++ aoc6/aoc6.py
@@ -1,26 +1,52 @@
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
-    def __repr__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
-
-    def __sub__(self, other):
-        return abs((self.x - other.x) + (self.y - other.y))
-
-def makepoints():
+def makepoints(data=None):
+  allpoints = []
+  if not data:
     with open('input') as fd:
-        data = fd.read().splitlines()
-    allpoints = []
+      data = fd.read().splitlines()
     for i in data:
-        x,y = int(i.split(', ')[0]), int(i.split(', ')[1])
-        allpoints.append(Point(x,y))
-    return allpoints
+      x,y = int(i.split(', ')[0]), int(i.split(', ')[1])
+      allpoints.append(Point(x,y))
+  else:
+    for i in data:
+      allpoints.append(Point(i[0],i[1]))
+  return allpoints
 
+class Point():
+  def __init__(self, x, y):
+    self.column = x
+    self.row = y
+
+  def __repr__(self):
+    return '(' + str(self.column) + ', ' + str(self.row) + ')'
+
+  def __sub__(self, other):
+    return abs((self.column - other.column) + (self.row - other.row))
+
+  def isinfinite(self, points):
+    pass
+
+  def __ge__(self, other):
+    return self.column>=other.column
+
+  def __gt__(self, other):
+    return self.column>other.column
+
+  def __lt__(self, other):
+    return self.column<other.column
+
+  def __le__(self, other):
+    return self.column<=other.column
+
+
+
+
+def maxcolumn(points):
+  return max(points)
 
 def mandist(a, b):
     return
 
 def closest(point):
     pass
+
--- aoc6/test_aoc6.py
+++ aoc6/test_aoc6.py
@@ -2,13 +2,25 @@ import aoc6
 import unittest
 
 class Testaoc6(unittest.TestCase):
-    def test_points(self):
-        pointa = aoc6.Point(1, 1)
-        pointb = aoc6.Point(1, 6)
-        self.assertEqual((pointa - pointb),5)
+  
+  def setUp(self):
+    self.cords = [(1,1), (1,6), (8,3), (3,4), (5,5), (8,9)]
 
-    def test_makepoints(self):
-        self.assertEqual(aoc6.makepoints()[0].x, 336)
+  def test_points(self):
+    pointa = aoc6.Point(1, 1)
+    pointb = aoc6.Point(1, 6)
+    self.assertEqual((pointa - pointb),5)
+
+  def test_makepoints(self):
+    self.assertEqual(aoc6.makepoints()[0].column, 336)
+
+  def test_maxcolumns(self):
+    points = aoc6.makepoints(self.cords)
+    self.assertEqual(aoc6.maxcolumn(points).column, 8)
+    
+  def test_maxrows(self):
+    result = max(self.cords, key=lambda item:item[1])[1]
+    self.assertEqual(result, 9)
 
 if __name__ == '__main__':
     unittest.main()
