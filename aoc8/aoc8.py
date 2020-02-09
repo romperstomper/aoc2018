59f5186b26ee8119fb576ebc21cf4f8b54fba158
gary <garyboland@gmail.com>
2020-02-09 19:40:28 +0000

    update

--- aoc8/aoc8.py
+++ aoc8/aoc8.py
@@ -1,11 +1,24 @@
 def metatree():
     input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
     input = input.split(' ')
-    return input
+    result = []
+    total = 0
+    first_header = Header(2, 3, [Header, Header], [1, 1, 2])
+    second_header = Header(0, 3, [],[10,11,12]) 
+    third_header = Header(0, 3, [], [2])
+    fourth_header = Header(0, 3, [], [99])
+    result = [first_header, second_header, third_header, fourth_header]
+    for i in result:
+        total += sum(i.metadatas)
+    return total
 
+
+def header_factory():
+    
+    return Header() 
 
 class Header():
-    def __init__(childnum, metadatanum, childnodes, metadatas):
+    def __init__(self, childnum, metadatanum, childnodes, metadatas):
         self.childnum = childnum
         self.metadatanum = metadatanum
         self.childnodes = childnodes
